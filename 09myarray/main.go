3fbea767dedcec82fbe4e363b9189a3c23cfb161
Rajdeep Sengupta <rajdeepsengupta@Rajdeeps-MacBook-Air.local>
2022-09-05 11:38:53 +0530

    Arrays Done

---
+++ 09myarray/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to go arrays!")
+
+	var fruitList [4]string
+
+	fruitList[0] = "Apple"
+	fruitList[1] = "Tomato"
+	fruitList[0] = "Apple"
+	fruitList[3] = "Orange"
+
+	fmt.Println("Fruit List is", fruitList)
+	fmt.Println("Fruit List is", len(fruitList))
+
+}
